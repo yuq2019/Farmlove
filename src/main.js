e6a67426477e57af4f0d0b9aa5d4d98df71cbf80
yuq2019 <649199392@qq.com>
2019-09-30 10:30:17 +0800

    first farm

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import '@/assets/css/reset.css'
+import '@/assets/js/rem.js'
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/router.js
+++ src/router.js
@@ -1,23 +1,32 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import HomePage from './views/HomePage'
+import Division from './views/Division'
+import ShopCart from './views/ShopCart'
+import Mine from './views/Mine'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path:'/homepage',
+      name:'homepage',
+      component:HomePage
+
+    },{
+      path:'/division',
+      name:'division',
+      component:Division
+    },{
+      path:'/shopcart',
+      name:'shopcart',
+      component:ShopCart
+    },{
+      path:'/mine',
+      name:'mine',
+      component:Mine
+
     }
   ]
 })
